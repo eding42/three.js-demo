24e72ecc577696d62b4bf52956af7102e07593fe
Edward Ding <eding2019@gmail.com>
2023-09-02 17:52:21 -0700

    testing testing! added more comments

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 
+// initializing the camera
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
